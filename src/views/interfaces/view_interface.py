9089fa2fb7a16d46afefdca01a1748452fcfa7f0
Fabricio da Silva Caetano <fscaetano@gmail.com>
2024-12-07 19:10:36 -0300

    fix: fixed typo in handle method

--- src/views/interfaces/view_interface.py
+++ src/views/interfaces/view_interface.py
@@ -5,5 +5,5 @@ from src.views.http_types.http_response import HttpResponse
 
 class ViewInterface(ABC):
     @abstractmethod
-    def handle(self, http_reques: HttpRequest) -> HttpResponse:
+    def handle(self, http_request: HttpRequest) -> HttpResponse:
         pass
