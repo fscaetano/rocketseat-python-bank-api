f07d7600c44694ab6aa62d43c880b8f3bb0dca7e
Fabricio da Silva Caetano <fscaetano@gmail.com>
2024-12-07 19:52:08 -0300

    feat: Implementing BalanceEditorView and LoginCreatorView

---
+++ src/views/balance_editor_view.py
@@ -0,0 +1,25 @@
+from src.controllers.interfaces.balance_editor import BalanceEditorInterface
+from src.views.http_types.http_request import HttpRequest
+from src.views.http_types.http_response import HttpResponse
+from .interfaces.view_interface import ViewInterface
+
+
+class BalanceEditorView(ViewInterface):
+    def __init__(self, controller: BalanceEditorInterface) -> None:
+        self.__controller = controller
+
+    def handle(self, http_request: HttpRequest):
+        user_id = http_request.params.get("user_id")
+        new_balance = http_request.body.get("new_balance")
+        self.__validate_inputs(user_id, new_balance)
+        response = self.__controller.edit(user_id, new_balance)
+        return HttpResponse(body={"data": response}, status_code=200)
+
+    def __validate_inputs(self, user_id, new_balance):
+        if (
+            not user_id
+            or not new_balance
+            or not isinstance(user_id, int)
+            or not isinstance(new_balance, float)
+        ):
+            raise Exception("Invalid input.")
