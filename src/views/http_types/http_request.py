e157b5f884304facb59d0b8d7ac9514e069df403
Fabricio da Silva Caetano <fscaetano@gmail.com>
2024-12-07 19:09:40 -0300

    fix: changed HttpRequest members to public

--- src/views/http_types/http_request.py
+++ src/views/http_types/http_request.py
@@ -7,10 +7,10 @@ class HttpRequest:
         token_infos: dict = None
     ) -> None:
 
-        self.__body = body
-        self.__headers = headers
-        self.__params = params
-        self.__token_infos = token_infos
+        self.body = body
+        self.headers = headers
+        self.params = params
+        self.token_infos = token_infos
 
 
 http_request = HttpRequest(body={"hello": "world"})
